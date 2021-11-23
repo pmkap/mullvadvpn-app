f2f42234276cc7f25edc2e654e13bbccf243d548
Emīls <emils@mullvad.net>
2021-11-23 14:25:27 +0000

    fixup! Add exclusion GID fetcher

--- mullvad-daemon/src/exclusion_gid.rs
+++ mullvad-daemon/src/exclusion_gid.rs
@@ -4,7 +4,7 @@ const EXCLUSION_GROUP: &[u8] = b"mullvad-exclusion\0";
 
 /// Returns the GID of `mullvad-exclusion` group if it exists.
 pub fn get_exclusion_gid() -> Option<u32> {
-    let exclusion_group_name = unsafe { CStr::from_ptr(EXCLUSION_GROUP.as_ptr() as *const i8) };
+    let exclusion_group_name = unsafe { CStr::from_bytes_with_nul_unchecked(EXCLUSION_GROUP) };
     talpid_core::macos::get_group_id(exclusion_group_name)
 }
 
